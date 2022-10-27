e872c98c8b6a11ddef8ce74e680155a19adfd277
a.yakimkin <ayakimkin@gmail.com>
2022-10-27 13:06:18 +0300

    iterator seq

--- src/iterators/seq/seq.ts
+++ src/iterators/seq/seq.ts
@@ -1,41 +1,36 @@
 
-export default function(...args: any) {//: IterableIterator<undefined> {
-  let index: number = 0;
-  let cursor:any;
-  let state = 0;
+// export default function<T extends Iterable<any>>(...args: T[]): T extends Iterable<infer V> ?
+//                                                               Iterable<V> : Iterable<unknown>     { // IterableIterator<undefined> {
+
+export default function(...args: any[]): IterableIterator<undefined> {
+  let cursor:any = null;
+  let argsCursor = args[Symbol.iterator]();
 
   return {
-    [Symbol.iterator]() { //: IterableIterator<undefined> {
+    [Symbol.iterator](): IterableIterator<undefined> {
       return this;
     },
-    next() { //: IteratorResult<undefined> {
-      if (args.length <= index) {
-        return { done: true, value: null }
-      }
 
-      if (state == 0) {
-        cursor = args[index][Symbol.iterator]();
-        state = 1;
-      }
+    next(): IteratorResult<undefined> {
+      if (!cursor) {
+        let test = argsCursor.next();
 
-      if (cursor == null) {
-        index++;
-      } else {
-        const res = cursor.next();
-        console.log("res.value", res.value);
-        console.log("res.done", String(res.done));
-
-        if (res.done) {
-          cursor = null;
-          index++;
-          state = 0;
-          // this.next();
-          return { done: false, value: "__" }
-        } else { //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-          // return res;
-          return { value: res.value, done: false }
+        if (test.done) {
+          return { done: true, value: null }
         }
+
+        cursor = test.value[Symbol.iterator]();
       }
+
+      const res = cursor.next();
+
+      if (res.done) {
+        cursor = null;
+
+        return this.next();
+      }
+
+      return res;
     }
   }
 }
