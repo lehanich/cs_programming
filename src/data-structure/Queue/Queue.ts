185c38d769447f341635321ed763357e76d60e85
a.yakimkin <ayakimkin@gmail.com>
2022-11-25 12:10:15 +0300

    queue refactor

--- src/data-structure/Queue/Queue.ts
+++ src/data-structure/Queue/Queue.ts
@@ -3,33 +3,33 @@ import {
   Queue as IQueue
 } from "./interface";
 import LinkedList from "../LinkedList/LinkedList";
+import LinkedListNode from "../LinkedList/LinkedListNode";
 import {
   ListNodeVal,
+  LinkedList as ILinkedList,
   ListNodeLink
 } from "../LinkedList/interface";
 
-export default class Queue<T> extends LinkedList<T> implements IQueue<T> {
+export default class Queue<T> implements IQueue<T> {
   public maxSize: number = 10;
   public length: number = 0;
-  public head: ListNodeLink<T> = null;
-  public rear: ListNodeLink<T> = null;
+  protected queue: LinkedList<T> = new LinkedList<T>;
+  public first: ListNodeLink<T> = null;
+  public last: ListNodeLink<T> = null;
 
   constructor(maxSize: number) {
-    super();
     this.maxSize = maxSize;
-    this.head = null;
-    this.rear = null;
+    this.first = null;
+    this.last = null;
   }
 
   public push(value: T): void {
-    if (this.length < this.maxSize) {
-      this.add(value)
 
-      if (!this.head) {
-        this.head = this.first;
-      }
+    if (this.length < this.maxSize) {
+      const newItem =  this.queue.add(value);
+
+      this.syncQueue();
       this.length++;
-      this.rear = this.last;
     }
   }
 
@@ -40,12 +40,79 @@ export default class Queue<T> extends LinkedList<T> implements IQueue<T> {
     }
 
     const value = this.first?.value;
-    const oldFirst = this.deleteFirst();
+    const oldFirst = this.queue.deleteFirst();
 
     this.length--;
-    this.head = this.first;
+    this.syncQueue();
 
     return <T>value;
   }
 
+  insertFirst(value: T): LinkedList<T> {
+    return this.queue.insertFirst(value);
+  }
+
+  deleteLast(): ListNodeLink<T> {
+    return this.queue.deleteLast();
+  }
+
+  deleteFirst(): ListNodeLink<T> {
+    return this.queue.deleteFirst();
+  }
+
+  *[Symbol.iterator](): Iterator<T> {
+    yield <T>this.first?.value;
+
+    let currentNode = this.first;
+
+    while (currentNode) {
+      currentNode = currentNode.next;
+      yield <T>currentNode?.value;
+    }
+  }
+
+  protected syncQueue() {
+    this.first = this.queue.first;
+    this.last = this.queue.last;
+  }
+
+  findContainer(key: any): ListNodeLink<T> {
+    let current: ListNodeLink<any> = <ListNodeLink<T>>this.queue.first;
+
+    if (!current) {
+      return undefined;
+    }
+
+    do {
+      if (current?.value![0] === key) {
+        return current;
+      }
+
+      current = current?.next;
+    } while (current && current!.next)
+
+    if (current?.value![0] === key) {
+      return current;
+    }
+
+    return undefined;
+  }
+
+  delete(link: ListNodeLink<T>): void {
+    if (this.last === link) {
+      this.deleteLast();
+    } else if (this.first === link) {
+      this.deleteFirst();
+    } else {
+      let prev = link?.prev;
+      let next = link?.next;
+
+      prev!.next = next;
+      next!.prev = prev;
+    }
+  }
+
+  find(value: T): ListNodeLink<T> {
+    return this.queue.find(value);
+  }
 }
